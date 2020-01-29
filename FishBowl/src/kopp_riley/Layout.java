3e5bc34afd75ace0df8cdf6402fc3be7cf3f3617
Riley Kopp <koppril7@gmail.com>
2020-01-29 16:01:38 -0700

    Added layout for the Info Bar

--- FishBowl/src/kopp_riley/Layout.java
+++ FishBowl/src/kopp_riley/Layout.java
@@ -2,10 +2,12 @@ package kopp_riley;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -13,17 +15,24 @@ public class Layout {
 
 
     // Action Bar Items
-    private String actionsList[] = { "GoldFish", "AngelFish", "Remove"};
+    private String    actionsList[] = { "GoldFish", "AngelFish", "Remove"};
     private ComboBox  actionSelection;
     private TextField feedAmount;
-    private Button feed;
+    private Button    feed;
 
+    // Info Bar Items
+    private Button newDayButton;
+    private Text   bowlInfo;
+    private Button small;
+    private Button medium;
+    private Button large;
 
     public Scene init(){
         Scene scene;
 
         BorderPane borderPane = new BorderPane();
-        borderPane.setBottom(new Label("Bottom"));
+        borderPane.setMinSize(700, 600);
+        borderPane.setBottom(setupInfoBar());
         borderPane.setLeft(setupActionBar());
 
         scene = new Scene(borderPane);
@@ -32,6 +41,7 @@ public class Layout {
 
     private VBox setupActionBar(){
         VBox side = new VBox();
+        side.setAlignment(Pos.CENTER);
         ObservableList list = side.getChildren();
 
         ObservableList<String> actions = FXCollections.observableArrayList(actionsList);
@@ -45,11 +55,38 @@ public class Layout {
 
         list.add(actionSelection);
         // TODO add R object here (See notes)
+        list.add(new Text("Feed Amount"));
         list.add(feedAmount);
         list.add(feed);
 
 
 
         return side;
+    }
+
+    private BorderPane setupInfoBar(){
+        BorderPane info = new BorderPane();
+
+
+        // Init Items
+        newDayButton = new Button("New Day");
+        bowlInfo     = new Text("TMP\nTMP\nTMP");
+        small        = new Button("3x3");
+        medium       = new Button("4x5");
+        large        = new Button("6x8");
+
+        HBox resizeOptions = new HBox();
+        resizeOptions.setAlignment(Pos.CENTER_LEFT);
+        resizeOptions.getChildren().addAll(small, medium, large);
+
+        BorderPane.setAlignment(bowlInfo, Pos.CENTER);
+        BorderPane.setAlignment(newDayButton, Pos.CENTER_LEFT);
+        //BorderPane.setAlignment(resizeOptions, Pos.CENTER_RIGHT);
+        info.setLeft(newDayButton);
+        info.setCenter(bowlInfo);
+        info.setRight(resizeOptions);
+
+
+        return info;
     }
 }
