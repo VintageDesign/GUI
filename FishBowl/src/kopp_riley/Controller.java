a20825fa5b412c6ad3379fa6eacad4393c1a28ec
Riley Kopp <koppril7@gmail.com>
2020-01-29 14:31:51 -0700

    Added the correct objects for the action bar

---
+++ FishBowl/src/kopp_riley/Controller.java
@@ -0,0 +1,15 @@
+package kopp_riley;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    // Action Bar Items
+    private ComboBox  actionSelection;
+    private TextField feedAmount;
+    private Button    feed;
+
+
+}
--- FishBowl/src/kopp_riley/Layout.java
+++ FishBowl/src/kopp_riley/Layout.java
@@ -1,19 +1,33 @@
 package kopp_riley;
 
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class Layout {
+
+
+
+
     public Scene init(){
         Scene scene;
 
         BorderPane borderPane = new BorderPane();
-        borderPane.setBottom(new Label("Test"));
+        borderPane.setBottom(setupActionBar());
         borderPane.setLeft(new Label("Side"));
 
         scene = new Scene(borderPane);
         return scene;
+    }
+
+    private VBox setupActionBar(){
+        VBox side = new VBox();
+
+        return side;
     }
 }
