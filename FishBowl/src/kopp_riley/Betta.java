29ad5a583ba858a177c5c5922656f16c6d97bf4b
Riley Kopp <koppril7@gmail.com>
2020-02-17 08:19:11 -0700

    Added comments to the Betta

--- FishBowl/src/kopp_riley/Betta.java
+++ FishBowl/src/kopp_riley/Betta.java
@@ -1,5 +1,12 @@
 package kopp_riley;
 
+
+/***********************************************************************************************
+ * @author Riley Kopp
+ * Betta Initialization
+ *
+ * This acts as the Pirrana Bonus assignment, I just wanted to be different and call it a Betta
+ **********************************************************************************************/
 public class Betta extends Fish {
 
     Betta(){
