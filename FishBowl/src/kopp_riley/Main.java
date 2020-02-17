5600ccc009bdf335a41b1e5433a0a2f8536b0d81
Riley Kopp <koppril7@gmail.com>
2020-02-17 08:38:30 -0700

    added comment block to main

--- FishBowl/src/kopp_riley/Main.java
+++ FishBowl/src/kopp_riley/Main.java
@@ -7,6 +7,23 @@ import javafx.stage.Stage;
 import javax.management.modelmbean.ModelMBean;
 import java.beans.PropertyChangeSupport;
 
+/***********************************************************************************************
+ * @author  Riley Kopp
+ * Descripton:
+ * The entry point of the Aquarium program. For more information see the assignment on D2L for
+ * a full description of the program.
+ *
+ * Last Tier Completed: 3
+ * Extensions Completed:
+ * +5 Resize buttons are all the same size
+ * +10 Pirrana moves randomly and kills fish it lands on.
+ *      - NOTE: The Pirrana was implemented as a Betta fish, as they both kill other fish in
+ *              life and because it is more likely that our users would own a betta rather than
+ *              a Pirrana. Plus I like making your life more difficult Yoyslin
+ *
+ * Known Bugs:
+ * None... I cleaned this tank thouroughly
+ **********************************************************************************************/
 public class Main extends Application {
     private Layout layout;
     private Controller controller;
