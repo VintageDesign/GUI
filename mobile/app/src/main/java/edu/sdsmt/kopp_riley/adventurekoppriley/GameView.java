20470a29fe40f1181f0e9f7253041cd4cdf90a3f
Riley Kopp <koppril7@gmail.com>
2020-04-24 17:30:00 -0600

    remote push

---
+++ mobile/app/src/main/java/edu/sdsmt/kopp_riley/adventurekoppriley/GameView.java
@@ -0,0 +1,34 @@
+package edu.sdsmt.kopp_riley.adventurekoppriley;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+
+
+    public GameView(Context context) {
+        super(context);
+
+        this.setBackgroundColor(Color.GRAY);
+    }
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.setBackgroundColor(Color.GRAY);
+    }
+
+    public GameView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.setBackgroundColor(Color.GRAY);
+    }
+
+    public GameView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        this.setBackgroundColor(Color.GRAY);
+    }
+}
